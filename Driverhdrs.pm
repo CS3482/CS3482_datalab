a708b2474f875105f75c2ee4c1c20590cabc932d
cindyanorris <can@cs.appstate.edu>
2021-02-04 14:48:28 -0500

    changed server port

--- Driverhdrs.pm
+++ Driverhdrs.pm
@@ -6,7 +6,7 @@ package Driverhdrs;
 
 $LAB = "datalab";
 $SERVER_NAME = "student2.cs.appstate.edu";
-$SERVER_PORT = 15001;
+$SERVER_PORT = 15003;
 $COURSE_NAME = "csapp";
 $AUTOGRADE_TIMEOUT = 0;
 1;
